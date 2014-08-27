4d7c2ef91577ebb13a64af7934e1f316533ab0b8
Vincent Rischmann <vincent@appgratis.com>
2014-08-27 12:14:32 +0200

    Store the expiry time in UTC directly

--- key.go
+++ key.go
@@ -15,7 +15,7 @@ func NewKeyObject(key interface{}, expiryTime int64) KeyObject {
 		Key: key,
 	}
 	if expiryTime >= 0 {
-		k.ExpiryTime = time.Unix(expiryTime/1000, 0)
+		k.ExpiryTime = time.Unix(expiryTime/1000, 0).UTC()
 	}
 
 	return k
@@ -27,7 +27,7 @@ func (k KeyObject) Expired() bool {
 
 func (k KeyObject) String() string {
 	if !k.ExpiryTime.IsZero() {
-		return fmt.Sprintf("KeyObject{ExpiryTime: %s, Key: %s}", k.ExpiryTime.UTC(), DataToString(k.Key))
+		return fmt.Sprintf("KeyObject{ExpiryTime: %s, Key: %s}", k.ExpiryTime, DataToString(k.Key))
 	}
 
 	return fmt.Sprintf("%s", DataToString(k.Key))
